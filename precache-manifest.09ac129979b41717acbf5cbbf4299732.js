a2a3713823346684a1cb04103bbe9139cdca74f4
Piyush <piyush.dongre16@gmail.com>
2020-09-28 22:14:44 +0530

    Updates

---
+++ precache-manifest.09ac129979b41717acbf5cbbf4299732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998436f316dfa1a16d4922538745178f",
+    "url": "/Todo_app_react/index.html"
+  },
+  {
+    "revision": "f665bb5fadcb7ebdcdb2",
+    "url": "/Todo_app_react/static/css/main.16f84639.chunk.css"
+  },
+  {
+    "revision": "7bb3491e602851d26c27",
+    "url": "/Todo_app_react/static/js/2.56491ad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo_app_react/static/js/2.56491ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f665bb5fadcb7ebdcdb2",
+    "url": "/Todo_app_react/static/js/main.037fead7.chunk.js"
+  },
+  {
+    "revision": "36d21cb717385a6a8920",
+    "url": "/Todo_app_react/static/js/runtime-main.e00c4e17.js"
+  }
+]);
